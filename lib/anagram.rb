626f1a23d676dfad4d1a3d696a37d79a858ab87a
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-10-21 13:59:24 +0300

    anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(matching_words)
+        matched_words = []
+        for word in matching_words do
+            if word.split("").sort == @word.split("").sort
+                matched_words.push(word)
+        end
+    end
+matched_words
+    end
+end
